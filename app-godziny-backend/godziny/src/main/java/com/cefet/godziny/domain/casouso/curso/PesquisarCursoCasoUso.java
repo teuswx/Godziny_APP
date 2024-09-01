a12d418d2044e630348249521c6db4d54aa446f8
Luan Gonçalves Santos <luanlulu2010@hotmail.com>
2024-09-01 17:00:40 -0300

    Added the authorization on Curso module

--- app-godziny-backend/godziny/src/main/java/com/cefet/godziny/domain/casouso/curso/PesquisarCursoCasoUso.java
+++ app-godziny-backend/godziny/src/main/java/com/cefet/godziny/domain/casouso/curso/PesquisarCursoCasoUso.java
@@ -4,9 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 import com.cefet.godziny.api.curso.CursoRecuperarDto;
+import com.cefet.godziny.constantes.usuario.EnumRecursos;
+import com.cefet.godziny.infraestrutura.exceptions.UsuarioNaoAutorizadoException;
 import com.cefet.godziny.infraestrutura.persistencia.curso.CursoEntidade;
 import com.cefet.godziny.infraestrutura.persistencia.curso.CursoRepositorioJpa;
+import com.cefet.godziny.infraestrutura.persistencia.usuario.UsuarioEntidade;
 import com.cefet.godziny.infraestrutura.rest.curso.CursoRestConverter;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -22,9 +28,18 @@ public class PesquisarCursoCasoUso {
     @NotNull(message = "O nome do curso é obrigatório")
     private String nome;
 
-    public void validarPesquisa() throws Exception {}
+    public void validarPesquisa() throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UsuarioEntidade userContext = (UsuarioEntidade) authentication.getPrincipal();
+        if(!userContext.getTipo().equals(EnumRecursos.ADM)){
+            throw new UsuarioNaoAutorizadoException();
+        }
+    }
     
     public Page<CursoRecuperarDto> pesquisarCursos(Pageable pageable) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UsuarioEntidade admin = (UsuarioEntidade) authentication.getPrincipal();
+
         Specification<CursoEntidade> specification = Specification.where(null);
     
         if (nome != null) {
@@ -35,6 +50,9 @@ public class PesquisarCursoCasoUso {
             specification = specification.and((root, query, criteriaBuilder) -> 
                 criteriaBuilder.like(root.get("sigla"), "%" + sigla + "%"));
         }
+
+        specification = Specification.where((root, query, criteriaBuilder) -> 
+            criteriaBuilder.equal(root.get("coordenador").get("matricula"), admin.getMatricula()));
 
         Page<CursoRecuperarDto> pageCursoRecuperarDto = cursoRepositorioJpa.listCursos(specification, pageable)
             .map(CursoRestConverter::EntidadeToCursoRecuperarDto);
